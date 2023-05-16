cdc44175260659c7b4ae80131af7e692672a0483
Agshinhummatov <agshinshh@code.edu.az>
2023-05-17 03:22:19 +0400

    Practices

---
+++ Api-Onion/API-Intro/Api-intro/Controllers/EmployeeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api_intro.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase  // contrelerde basde retrun view elemek olmur
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            string[] names = { "Eli", "Aqsin", "Elcan" };
+
+            return Ok(names);
+        }
+
+
+        [HttpGet]
+        public IActionResult GetById( int? id)
+        {
+            if (id == null) return BadRequest();
+            return Ok("Eli -" + " " + id);
+
+        }
+        
+
+    }
+}
